d3cad5741c5539d28d48cf69bfb0193b556214f5
Filomeno Merlath <filo.meno.junior@hotmail.com>
2021-11-04 16:05:01 +0000

    database

---
+++ Database/populate.sql
@@ -0,0 +1,30 @@
+--medalhas
+insert into medals (medal_type, medal_level) values('km','Cobre');
+insert into medals (medal_type, medal_level) values('km','Prata');
+insert into medals (medal_type, medal_level) values('km','Ouro');
+insert into medals (medal_type, medal_level) values('km','Platina');
+insert into medals (medal_type, medal_level) values('km','Diamante');
+
+insert into medals (medal_type, medal_level) values('Diferentes campos visitados','Cobre');
+insert into medals (medal_type, medal_level) values('Diferentes campos visitados','Prata');
+insert into medals (medal_type, medal_level) values('Diferentes campos visitados','Ouro');
+insert into medals (medal_type, medal_level) values('Diferentes campos visitados','Platina');
+insert into medals (medal_type, medal_level) values('Diferentes campos visitados','Diamante');
+
+insert into medals (medal_type, medal_level) values('Jogos Completados','Cobre');
+insert into medals (medal_type, medal_level) values('Jogos Completados','Prata');
+insert into medals (medal_type, medal_level) values('Jogos Completados','Ouro');
+insert into medals (medal_type, medal_level) values('Jogos Completados','Platina');
+insert into medals (medal_type, medal_level) values('Jogos Completados','Diamante');
+
+insert into medals (medal_type, medal_level) values('Jogos ganhos','Cobre');
+insert into medals (medal_type, medal_level) values('Jogos ganhos','Prata');
+insert into medals (medal_type, medal_level) values('Jogos ganhos','Ouro');
+insert into medals (medal_type, medal_level) values('Jogos ganhos','Platina');
+insert into medals (medal_type, medal_level) values('Jogos ganhos','Diamante');
+
+insert into cities(city_name) values('Lisboa');
+insert into cities(city_name) values('Porto');
+insert into cities(city_name) values('Braga');
+insert into cities(city_name) values('Coimbra');
+insert into cities(city_name) values('Aveiro');
---
+++ Database/query.sql
@@ -0,0 +1,4 @@
+select * from players where player_email = 'filo.meno.junior@hotmail.com' and player_password = 'filomeno 123'
+;
+
+--insert into players(player_firstname, player_lastname,player_email,player_password,player_bdate,player_gender,player_address,city_fk_id) values('Filomeno','Merlath','filo.meno.junior@hotmail.com','iade123','01.01.1995','M','Rua dos pupilos 11','2');
