2a44695c33a1d1ea43ad22d30290678ae9c17f75
Filomeno Merlath <filo.meno.junior@hotmail.com>
2021-11-08 16:04:37 +0000

    database
    heroku database

--- models/connection.js
+++ models/connection.js
@@ -1,15 +1,16 @@
 var pg = require('pg');
 
+const connectionString = "postgres://ubpolgaj:gYelANU6vPA4bHDqMkm82x59wa40IAgW@surus.db.elephantsql.com/ubpolgaj"
 //const connectionString = "postgres://postgres:junior@localhost:5432/softpadel"
-const connectionString = "postgres://postgres:password@localhost:5432/softpadel"
+//const connectionString = "postgres://postgres:password@localhost:5432/softpadel"
 const Pool = pg.Pool
 const pool = new Pool({
     connectionString,
     max: 10,
-    /*ssl: {
+    ssl: {
         require: true, 
         rejectUnauthorized: false
-    }*/
+    }
 })
 
 module.exports = pool;
