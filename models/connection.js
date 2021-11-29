b0f2f199ebbea6b3703d7314d274517093616784
filomeno merlath <filo.meno.junior@hotmail.com>
2021-11-29 14:23:00 +0000

    update

--- models/connection.js
+++ models/connection.js
@@ -6,10 +6,10 @@ const Pool = pg.Pool;
 const pool = new Pool({
   connectionString,
   max: 10,
-  /*ssl: {
-        require: true, 
-        rejectUnauthorized: false
-    }*/
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
