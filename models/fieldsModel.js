314a7b8f4e5ac474d6db45da13b69d5fdf6a9874
William Fejó <dkamino53@gmail.com>
2021-11-30 16:47:20 +0000

    Revert "Revert "Update 4.1""
    This reverts commit 28b2e90b457b26e8366cd099b786fa8e17fcb775.
    
    # Conflicts:
    #	models/connection.js
    #	models/fieldsModel.js
    #	public/fields.html
    #	public/index.html
    #	public/javascripts/fields.js
    #	public/javascripts/sign.js
    #	public/stylesheets/style.css
    #	public/stylesheets/userSign.css
    #	routes/fieldsRouter.js

--- models/fieldsModel.js
+++ models/fieldsModel.js
@@ -1,13 +1,24 @@
 var pool = require("./connection");
 
-module.exports.getAllfields = async function () {
-  try {
-    let sql = "select * from fields";
-    let result = await pool.query(sql);
-    let fields = result.rows;
-    return { status: 200, result: fields };
-  } catch (error) {
-    console.log(error);
-    return { status: 500, result: error };
-  }
-};
+module.exports.getAllfields = async  function() {
+    try {
+        let sql = "select * from fields";
+        let result = await pool.query(sql);
+        let fields = result.rows;
+        return {status:200, result: fields};
+    } catch (error) {
+        console.log(error);
+        return{status:500, result:error};
+    }
+}
+module.exports.registerField = async function(newField){
+    try {
+        let sql="insert into fields (field_name, field_address, field_number, field_open, field_close, field_price_h, field_location, city_fk_id) values($1, $2, $3, $4, $5, $6, $7, $8) ";
+        let result = await pool.query(sql, [newField.name, newField.address, newField.number, newField.openedTime, newField.closedTime, newField.price, newField.location, newField.cityid]);
+        return{ status: 200, result: result};
+    } catch (error) {
+        console.log(error);
+        return{ status: 500, result: error};
+    }
+}
+}
