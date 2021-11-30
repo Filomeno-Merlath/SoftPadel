18c9753e77ae2cb563e02b2b992c40fd623cc902
filomeno merlath <filo.meno.junior@hotmail.com>
2021-11-30 22:13:21 +0000

    update

--- public/javascripts/sign.js
+++ public/javascripts/sign.js
@@ -40,8 +40,7 @@ async function newUser() {
     .then(async (element) => {
       console.log(element);
       try {
-        let data;
-        data = {
+        let data = {
           firstName: document.getElementById("fname").value,
           lastName: document.getElementById("lname").value,
           username: document.getElementById("uname").value,
@@ -64,6 +63,51 @@ async function newUser() {
         console.log(JSON.stringify(result));
         window.alert("Registration successful!");
         window.location = "userLogin.html";
+      } catch (error) {
+        console.log(error);
+        return;
+      }
+    })
+    .catch((err) => console.log(err));
+}
+async function newField() {
+  let address = document.querySelector("#address").value;
+  let sel = document.querySelector("#cities");
+  let city = sel.options[sel.selectedIndex].text;
+  var url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    " " +
+    city +
+    ".json?limit=1&access_token=pk.eyJ1IjoiZmlsb21lbm9tIiwiYSI6ImNrdjEyaGg1NDBjdXkydW92dHVib2RtbXUifQ.YgXHEY0WXf8ptKQJ1wkUyQ";
+  console.log(url);
+
+  fetch(url)
+    .then((response) => response.json())
+    .then(async (element) => {
+      console.log(element);
+      try {
+        let data = {
+          name: document.getElementById("fname").value,
+          address: document.getElementById("address").value,
+          number: document.getElementById("number").value,
+          openedTime: document.getElementById("opened").value,
+          closedTime: document.getElementById("closed").value,
+          price: document.getElementById("price").value,
+          cityid: parseInt(document.getElementById("cities").value),
+          location: `${element.features[0].geometry.coordinates[1]},${element.features[0].geometry.coordinates[0]}`,
+        };
+        console.log(data);
+        let result = await $.ajax({
+          url: `/api/fields`,
+          method: "post",
+          data: JSON.stringify(data),
+          dataType: "json",
+          contentType: "application/json",
+        });
+        console.log(JSON.stringify(result));
+        window.alert("Registration successful!");
+        window.location = "index.html";
       } catch (error) {
         console.log(error);
         return;
