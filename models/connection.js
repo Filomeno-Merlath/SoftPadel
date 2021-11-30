28b2e90b457b26e8366cd099b786fa8e17fcb775
William Fejó <dkamino53@gmail.com>
2021-11-30 16:34:32 +0000

    Revert "Update 4.1"
    This reverts commit 207dcfd1b84d532b0df0d38e0ab75561a5c378b6.

--- models/connection.js
+++ models/connection.js
@@ -1,14 +1,16 @@
 var pg = require('pg');
 
-const connectionString = "postgres://bzvfvvqu:hpNSZKZrOBiAlCdQn0_b4I0z_pknn8_d@surus.db.elephantsql.com/bzvfvvqu"
+const connectionString = "postgres://hirtpgpu:x5bIxEbHrAire_U8iNQOrSKEaJV1ZzXP@surus.db.elephantsql.com/hirtpgpu"
+//const connectionString = "postgres://postgres:junior@localhost:5432/softpadel"
+//const connectionString = "postgres://postgres:password@localhost:5432/softpadel"
 const Pool = pg.Pool
 const pool = new Pool({
     connectionString,
     max: 10,
-    ssl: {
+    /*ssl: {
         require: true, 
         rejectUnauthorized: false
-    }
+    }*/
 })
 
 module.exports = pool;
